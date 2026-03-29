2976587f1950c3c964ffa203669d52890ec77bfa
mikesk8r <mikesk8r@noreply.codeberg.org>
2026-03-29 12:40:59 -0400

    add support for older vtfs and mipmaps

--- headcrab-vtf/src/dxt.rs
+++ headcrab-vtf/src/dxt.rs
@@ -1,14 +1,16 @@
-pub fn decode_dxt(version: usize, data: &[u8], width: usize, height: usize) -> Vec<u8> {
+use super::ImageDataFormat;
+
+pub fn decode_dxt(version: ImageDataFormat, data: &[u8], width: usize, height: usize) -> Vec<u8> {
     let mut output: Vec<u8> = vec![0; width * height * 4];
 
     match version {
-        1 => {
+        ImageDataFormat::DXT1 => {
             texpresso::Format::Bc1.decompress(data, width, height, output.as_mut_slice());
         }
-        3 => {
+        ImageDataFormat::DXT3 => {
             texpresso::Format::Bc2.decompress(data, width, height, output.as_mut_slice());
         }
-        5 => {
+        ImageDataFormat::DXT5 => {
             texpresso::Format::Bc3.decompress(data, width, height, output.as_mut_slice());
         }
         _ => {}

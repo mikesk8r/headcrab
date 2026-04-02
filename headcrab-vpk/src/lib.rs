3ea4d5f8b9d122e4fea85f81bd11903b1f892c93
mikesk8r <92176838+mikesk8r@users.noreply.github.com>
2026-04-01 21:44:25 -0400

    vpk types

--- headcrab-vpk/src/lib.rs
+++ headcrab-vpk/src/lib.rs
@@ -1,14 +1,28 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug, Default)]
+pub struct VPK {
+    pub version: u8,
+    pub entries: Vec<VPKEntry>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Clone, Debug)]
+pub enum EntryType {
+    VTF,
+    // add mdl types
+    KeyValues1,
+    KeyValues2,
+    KeyValues3,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Clone, Debug)]
+pub struct VPKEntry {
+    pub entry_type: EntryType,
+    file: String,
+    offset: u32,
+}
+
+impl VPKEntry {
+    pub fn read() /* -> Vec<u8> */
+    {
+        // TODO
     }
 }
